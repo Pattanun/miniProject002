51dad7fb2a674b4a01c461f2ae4226c1596ecf33
Pattanun <oatinfinity42@gmail.com>
2022-04-20 17:25:09 +0700

    edit4

--- frontend/pages/index copy.js
+++ frontend/pages/index copy.js
@@ -1,27 +1,27 @@
-import Head from 'next/head' 
-import Layout from '../components/layout' 
-import Navbar from '../components/navbar'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head' 
+// import Layout from '../components/layout' 
+// import Navbar from '../components/navbar'
+// import styles from '../styles/Home.module.css'
 
-export default function Home({ token }) {
+// export default function Home({ token }) {
  
-  return (
-    <Layout>
-    <Head>
-        <title>First Page</title>
-    </Head>
-    <div className={styles.container}>
-        <Navbar />
-        <div>
-        <h1>Welcome to Guitar shop</h1>
-        </div>
-    </div>
-</Layout>
-  )
-}
+//   return (
+//     <Layout>
+//     <Head>
+//         <title>First Page</title>
+//     </Head>
+//     <div className={styles.container}>
+//         <Navbar />
+//         <div>
+//         <h1>Welcome to Guitar shop</h1>
+//         </div>
+//     </div>
+// </Layout>
+//   )
+// }
 
-export function getServerSideProps({ req, res }) {
-  // console.log("token from cookie: ",cookie.get("token")) 
-  // console.log('req: ', req.headers)
-  return { props: { token: req.cookies.token || "" } };
-}
+// export function getServerSideProps({ req, res }) {
+//   // console.log("token from cookie: ",cookie.get("token")) 
+//   // console.log('req: ', req.headers)
+//   return { props: { token: req.cookies.token || "" } };
+// }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,19 +9,12 @@ export default function Home() {
   return (
       <Navbar />
   )
-
-  // return (
-  //   <div className="bg-black">
-  //   <h1>Welcome to SHOP GUITAR</h1>
-  //   </div>
-  // )
-
 }
 
 
 
-export function getServerSideProps({ req, res }) {
-  // console.log("token from cookie: ",cookie.get("token")) 
-  // console.log('req: ', req.headers)
-  return { props: { token: req.cookies.token || "" } };
-}
+// export function getServerSideProps({ req, res }) {
+//   // console.log("token from cookie: ",cookie.get("token")) 
+//   // console.log('req: ', req.headers)
+//   return { props: { token: req.cookies.token || "" } };
+// }
